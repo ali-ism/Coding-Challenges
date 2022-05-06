968dd3f2c5b2eff623195e0cd5d13a9e6a686d34
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-05-06 13:03:51 +0300

    Create tree_preorder_traversal.cpp

---
+++ hackerrank/tree_preorder_traversal.cpp
@@ -0,0 +1,23 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+void preOrder(Node *root)
+{
+    cout << root->data << " ";
+    if(root->left) preOrder(root->left);
+    if(root->right) preOrder(root->right);
+}
