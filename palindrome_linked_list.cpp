067dd5559f1be0a79e634bad6e2e12bb2befb38c
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-05-06 12:24:43 +0300

    Create palindrome_linked_list.cpp

---
+++ palindrome_linked_list.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head)
+    {
+        deque<int> dq;
+        ListNode* ptr = head;
+        while(ptr)
+        {
+            dq.push_front(ptr->val);
+            ptr = ptr->next;
+        }
+        while(dq.size()>1)
+        {
+            if(dq.front()!=dq.back()) return false;
+            dq.pop_front();
+            dq.pop_back();
+        }
+        return true;
+    }
+};
