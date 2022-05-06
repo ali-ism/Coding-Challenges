73b6044705b657988c3a3cf34fe0ec70fe434d34
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-05-06 12:53:40 +0300

    Create print_reverse_linked_list.cpp

---
+++ hackerrank/print_reverse_linked_list.cpp
@@ -0,0 +1,33 @@
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+using namespace std;
+
+void reversePrint(SinglyLinkedListNode* llist)
+{
+    stack<int> s;
+    SinglyLinkedListNode* current = llist;
+    while(current != nullptr)
+    {
+        s.push(current->data);
+        current = current->next;
+    }
+    while(!s.empty())
+    {
+        cout << s.top() << "\n";
+        s.pop();
+    }
+}
