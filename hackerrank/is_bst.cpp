60dedc2e2b8acc4cc102f2b6135c6fc7c697929a
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-05-06 13:11:30 +0300

    Create is_bst.cpp

---
+++ hackerrank/is_bst.cpp
@@ -0,0 +1,23 @@
+#include<climits>
+
+
+The Node struct is defined as follows:
+	struct Node {
+		int data;
+		Node* left;
+		Node* right;
+	}
+*/
+
+bool checkBST(Node* root, int min, int max)
+{
+    if (!root) return true;
+    if (root->data < min || root->data > max) return false;
+    return checkBST(root->left, min, root->data - 1) 
+        &&  checkBST(root->right, root->data + 1, max);
+}
+
+bool checkBST(Node* root)
+{
+    return checkBST(root, INT_MIN, INT_MAX);
+}
