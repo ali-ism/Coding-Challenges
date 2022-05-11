db0b45896a89c8198c77b8ebad469a77fa692fa5
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-05-11 22:14:19 +0300

    Create valid_sudoku.cpp

---
+++ leetcode/valid_sudoku.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(std::vector<std::vector<char>>& board)
+    {
+        int rows[9][9] = {0};
+        int cols[9][9] = {0};
+        int blocks[3][3][9] = {0};
+
+        for(int r=0; r<9; ++r)
+        {
+            for(int c=0; c<9; ++c)
+            {
+                if(board[r][c]!='.')
+                {
+                    int num = board[r][c]-'1';
+                    if(cols[num][c]++ || rows[r][num]++ || blocks[r/3][c/3][num]++)
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
